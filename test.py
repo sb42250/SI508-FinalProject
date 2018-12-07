bb7c2d346caf40991ee1edfe4570fdc72f005c5a
sb42250 <44405526+sb42250@users.noreply.github.com>
2018-12-07 17:04:31 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 18:06:08 2018
+
+@author: yyjia
+"""
+
+from finalproject import *
+import unittest
+import sys
+
+class MovieClassTests(unittest.TestCase):
+    def test_movie_title(self):
+        self.assertEqual(movieexample.title,"Titanic")
+    def test_movie_rating(self):
+        self.assertEqual(movieexample.rating,"7.8")
+    def test_movie_director(self):
+        self.assertEqual(movieexample.director,"James Cameron")
+
+class TweeterClassTests(unittest.TestCase):   
+    def test_tweet_id(self):
+        self.assertEqual(tweets[0].id,"1070130921825816576")
+    def test_tweet_retweets(self):
+        self.assertEqual(tweets[0].retweets,0)
+    def test_tweet_favorites(self):
+        self.assertEqual(tweets[0].favorites,198848)
+
+class DBTests(unittest.TestCase):
+    def test_title(self): 
+        self.assertEqual(title,"Titanic")
+    def test_rating(self):
+        self.assertEqual(movie_rating,[['7.8']])
+    def test_most_favorite_text(self):
+        self.assertEqual(most_favorite_text, [['King of the world!\n\n(Alternately, "Titanic\'s box office receipts")\n\n#SuccessIn4Words https://t.co/ZA1hKbc4gz']])
+    def test_insert_Tweeter(self):
+        self.assertEqual(res, True)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
---
+++ twitter_info.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 20:20:25 2018
+
+@author: yyjia
+"""
+
+import tweepy  
+consumer_key = "0EFF3gpGdni9fqhGx0fXps0LJ"  
+consumer_secret = "Kzs0ahHp7CObmE5uyrep1vQNkgL5b2D0fmQAQQQ18BKQWgYVQ4"  
+access_token = "1069394347429376006-OCRXYEElvEhrZuxsAi99cy1VFE8FR4"  
+access_token_secret = "JRerJTMNXPOawuReGc51mkY8olzGteVvkFmTndvSqooFW"
